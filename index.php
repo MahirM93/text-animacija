a803603e292a8cc8d90b4507eeec7a93e8e2445e
rails s <mesanovicmahir@gmail.com>
2022-08-28 12:11:40 +0200

    init

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<link rel="stylesheet"href="style.css">
+
+
+<header>
+
+</header>
+<body>
+<section>
+<div class="velika">
+    <h2>Ovo je tekst</h2>
+    <h4>Ovo je drugi tekst</h4>
+    <p>Ovo je treci tekst</p>
+</div>
+
+</section>
+
+</body>
+
+
+</html>
